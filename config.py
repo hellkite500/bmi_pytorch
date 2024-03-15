98afd86f5fef696c1a744d49fc22e43e9f975822
taddyb <tkb5476@psu.edu>
2024-03-14 21:21:29 -0400

    added union for python 3.9

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import logging
-from typing import List
+from typing import List, Union
 
 from pydantic import BaseModel, Field
 
@@ -9,6 +9,6 @@ log = logging.getLogger(__name__)
 class Config(BaseModel):
     input_size: int = 1
     output_size: int = 1
-    hidden_size: List[None | int] = Field(default_factory=lambda: [10, 10])
+    hidden_size: List[Union[None, int]] = Field(default_factory=lambda: [10, 10])
     learning_rate: float = 0.005
     epochs: int = 800
