927bbf68098f27a455be9623e928f7f14c82947b
Nels <hellkite500@gmail.com>
2024-03-15 11:11:36 -0700

    Revert "Initial bmi complete (#6)"
    This reverts commit 9332b53949d63ec83144ebbb3906eab421b248f9.

--- bmi_model.py
+++ bmi_model.py
@@ -1,15 +1,13 @@
 import torch
 from bmipy import Bmi
-from src.bmi_minimal import Bmi_Minimal
 from torch import Tensor
 
 from bmi_grid import Grid, GridType
 from config import Config
 from model import Model
 
-from typing import Tuple, List
 
-class Bmi_Model(Bmi_Minimal):
+class Bmi_Model(Bmi):
     """BMI composition wrapper for Model
 
     Args:
@@ -20,16 +18,15 @@ class Bmi_Model(Bmi_Minimal):
         super(Bmi_Model, self).__init__()
         # Grid 0 is a 0 dimension "grid" for scalars
         self.grid_0: Grid = Grid(0, 0, GridType.scalar)
-        self.input_names: Tuple[str] = ("precipitation",)
-        self.output_names: Tuple[str] = ("runoff",)
+        self.input_names = ["precipitation"]
+        self.output_names = ["runoff"]
         # all inputs and outputs map to scalar grid
         self.grid_map = {k: self.grid_0 for k in self.input_names + self.output_names}
-        self._grids: List[Grid] = [self.grid_0]
 
         self.units = {k: "-" for k in self.input_names + self.output_names}
 
         self.input = Tensor()
-        self.output = Tensor()
+        self.ouptut = Tensor()
         self._values = {}
         for name in self.input_names:
             self._values[name] = self.input
@@ -44,15 +41,13 @@ class Bmi_Model(Bmi_Minimal):
         """
         _config = Config()  # Hack to get the default values. We can change later
         self.model = Model(_config)
-        # TODO should these be attributes of the Bmi_Model, or the underlying Model?
         self.learning_rate = _config.learning_rate
-        self.optimizer = torch.optim.SGD(self.model.parameters(), self.learning_rate)
+        self.optimzer = torch.optim.SGD(self.model.parameters(), self.learning_rate)
 
     def update(self):
-        """Update the model for the internal timestep duration
-        """
+        """__sammary"""
         self.output = self.model(self.input)
 
     def finalize(self):
-        """Clean up any internal resources of the model"""
+        """_summary_"""
         pass
