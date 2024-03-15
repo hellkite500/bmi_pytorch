9332b53949d63ec83144ebbb3906eab421b248f9
Nels <nfrazier@lynker.com>
2024-03-15 11:51:23 -0600

    Initial bmi complete (#6)
    * feat: implement update_until and cleanup model control docstrings
    
    * test: test bmi module construction and initialize function
    
    * fix: bmi model construction and initialize
    
    * fix: use ParameterList so parameters are recorded properly, required for use in optimizer
    
    * feat(BMI): intermediate BMI class for building a minimal BMI model
    
    * feat: use Bmi_Minimal to implement bmi model

--- bmi_model.py
+++ bmi_model.py
@@ -1,13 +1,15 @@
 import torch
 from bmipy import Bmi
+from src.bmi_minimal import Bmi_Minimal
 from torch import Tensor
 
 from bmi_grid import Grid, GridType
 from config import Config
 from model import Model
 
+from typing import Tuple, List
 
-class Bmi_Model(Bmi):
+class Bmi_Model(Bmi_Minimal):
     """BMI composition wrapper for Model
 
     Args:
@@ -18,15 +20,16 @@ class Bmi_Model(Bmi):
         super(Bmi_Model, self).__init__()
         # Grid 0 is a 0 dimension "grid" for scalars
         self.grid_0: Grid = Grid(0, 0, GridType.scalar)
-        self.input_names = ["precipitation"]
-        self.output_names = ["runoff"]
+        self.input_names: Tuple[str] = ("precipitation",)
+        self.output_names: Tuple[str] = ("runoff",)
         # all inputs and outputs map to scalar grid
         self.grid_map = {k: self.grid_0 for k in self.input_names + self.output_names}
+        self._grids: List[Grid] = [self.grid_0]
 
         self.units = {k: "-" for k in self.input_names + self.output_names}
 
         self.input = Tensor()
-        self.ouptut = Tensor()
+        self.output = Tensor()
         self._values = {}
         for name in self.input_names:
             self._values[name] = self.input
@@ -41,13 +44,15 @@ class Bmi_Model(Bmi):
         """
         _config = Config()  # Hack to get the default values. We can change later
         self.model = Model(_config)
+        # TODO should these be attributes of the Bmi_Model, or the underlying Model?
         self.learning_rate = _config.learning_rate
-        self.optimzer = torch.optim.SGD(self.model.parameters(), self.learning_rate)
+        self.optimizer = torch.optim.SGD(self.model.parameters(), self.learning_rate)
 
     def update(self):
-        """__sammary"""
+        """Update the model for the internal timestep duration
+        """
         self.output = self.model(self.input)
 
     def finalize(self):
-        """_summary_"""
+        """Clean up any internal resources of the model"""
         pass
