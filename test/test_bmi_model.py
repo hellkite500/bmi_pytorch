55a5e0b0520efbcbab13f0d5b4e4df7de644c69d
hellkite500 <nfrazier@lynker.com>
2024-03-15 18:45:57 -0600

    feat: bmi get_var_itemsize

--- test/test_bmi_model.py
+++ test/test_bmi_model.py
@@ -130,3 +130,21 @@ def test_bmi_get_var_ptr_1(bmi_model_initialized, input):
     
     for expected, val in zip( m.input.flatten(), expected ):
         assert expected == val
+
+@pytest.mark.parametrize("input", [
+                          Tensor([0.0, 0]),
+                          Tensor([0, 1.0, 2]),
+                          Tensor([2, 1, 0]),
+                          Tensor([ [0] ]),
+                          Tensor([ [0, 1.0] ]),
+                          Tensor([ [0, 1.0], [2, 3.0] ]),
+                          Tensor([ [3, 2], [1.0, 0] ])
+                        ])
+def test_bmi_get_var_itemsize(bmi_model_initialized, input):
+    name = "precipitation"
+    m = bmi_model_initialized
+    m.input = input
+    m._values[name] = m.input
+    data = m.get_var_itemsize(name)
+
+    assert input.itemsize == data
